678612ced521f3783a0f523a5c86f987c44979f5
Hoàng Túy Minh <tuyminh.hoang@gmail.com>
2025-04-07 19:27:37 +0700

    set other data for image details (not finished)
    *tagsList changed to a GridView

--- AlbumAppGroup5/app/src/main/java/com/example/albumappgroup5/activities/ImageDetailsActivity.java
+++ AlbumAppGroup5/app/src/main/java/com/example/albumappgroup5/activities/ImageDetailsActivity.java
@@ -4,18 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 
 import com.example.albumappgroup5.R;
 import com.example.albumappgroup5.models.ImageDetailsObject;
+import com.example.albumappgroup5.models.TagObject;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ImageDetailsActivity extends Activity {
     String imageID;
@@ -24,11 +30,14 @@ public class ImageDetailsActivity extends Activity {
     EditText nameEdit, descriptionEdit, locationEdit;
     AutoCompleteTextView addTagEdit;
     TextView dateAdded;
-    ListView tagsList;
+    GridView tagsList;
     Button tagAddButton, updateDetailsButton;
 
     // database object
     DatabaseHandler database;
+
+    // data variables from database
+    List<TagObject> tagData;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -69,5 +78,29 @@ public class ImageDetailsActivity extends Activity {
         nameEdit.setText(details.getImageName());
         descriptionEdit.setText(details.getDescription());
         locationEdit.setText(details.getLocation());
+        Date dateDetails = details.getTimeAdded();
+        if (dateDetails == null)
+            dateAdded.setText(R.string.dateDefault);
+        else
+            dateAdded.setText(DateFormat.getDateInstance(DateFormat.MEDIUM).format(dateDetails));
+
+        tagData = database.getImageTagList(imageID);
+        ArrayAdapter<String> adapter;
+        if (tagData.isEmpty()) {
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new String[]{getResources().getString(R.string.tagDefault)});
+        }
+        else {
+            List<String> tagNameList = new ArrayList<>();
+            for (TagObject item : tagData) {
+                tagNameList.add(item.getTagName());
+            }
+            adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tagNameList);
+        }
+        tagsList.setAdapter(adapter);
+
+        tagsList.setOnItemClickListener((parent, view, position, id) -> {
+            // set item for deletion here
+        });
+
     }
 }
